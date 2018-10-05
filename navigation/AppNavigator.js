3bd7b3cd7b3a9151e83e1104afff561b909a03e7
Tatu Alapoikela <tatu.alapoikela@meiko.fi>
2018-10-05 23:49:28 +0300

    FishingScreen WIP

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,11 +1,13 @@
 import {createStackNavigator} from 'react-navigation';
 import MapScreen from '../screens/MapScreen';
 import GameScreen from '../screens/GameScreen';
+import FishingScreen from '../screens/FishingScreen';
 
 export default AppNavigator = createStackNavigator(
   {
     Map: MapScreen,
     Game: GameScreen,
+    FishingScreen: FishingScreen
   },
   {
     initialRouteName: 'Map',
